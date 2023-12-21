0bdb7cd0c8dc2d09eb493819eaaa64519d322adc
Luan Louzada <luanlouzadadev@gmail.com>
2023-12-20 23:14:04 -0300

    FAQ page use dynamically

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/luanlouzada/golangweb/views"
@@ -9,5 +10,26 @@ import (
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "What is the meaning of life?",
+			Answer:   "42",
+		}, {
+			Question: "What is the meaning of life?",
+			Answer:   "42",
+		}, {
+			Question: "What is the meaning of life?",
+			Answer:   "42",
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
