115fcb2eb1441b2d6fc47a9cccdd307aa3c4abc3
Luan Louzada <luanlouzadadev@gmail.com>
2023-12-31 08:59:04 -0300

    ---

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 
 func main() {
 	r := chi.NewRouter()
+
 	r.Get("/", controllers.StaticHandler(
 		views.Must(views.ParseFS(templates.FS,
 			"home.gohtml", "tailwind.gohtml"))))
@@ -23,8 +24,12 @@ func main() {
 	r.Get("/faq", controllers.FAQ(
 		views.Must(views.ParseFS(templates.FS,
 			"faq.gohtml", "tailwind.gohtml"))))
-	r.Get("/signup", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS,
-		"signup.gohtml", "tailwind.gohtml"))))
+
+	usersC := controllers.Users{}
+	usersC.Templates.New = views.Must(views.ParseFS(templates.FS,
+		"signup.gohtml", "tailwind.gohtml"))
+	r.Get("/signup", usersC.New)
+	r.Post("/users", usersC.Create)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page Not Found", http.StatusNotFound)
 	})
